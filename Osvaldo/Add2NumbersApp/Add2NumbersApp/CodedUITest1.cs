22b3c75f2d7bda0c16eb03f294ea37b6bcc77833
Osvaldo Zurita <ogzmlb01@gmail.com>
2018-05-25 18:25:50 -0400

    Added the CalculatorApp and Add2NumbersApp performed with Automation in Visual Studio.

---
+++ Osvaldo/Add2NumbersApp/Add2NumbersApp/CodedUITest1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Windows.Input;
+using System.Windows.Forms;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UITest.Extension;
+using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
+using System.CodeDom.Compiler;
+using Microsoft.VisualStudio.TestTools.UITesting.WinControls;
+using Mouse = Microsoft.VisualStudio.TestTools.UITesting.Mouse;
+using MouseButtons = System.Windows.Forms.MouseButtons;
+
+namespace Add2NumbersApp
+{
+    /// <summary>
+    /// Summary description for CodedUITest1
+    /// </summary>
+    [CodedUITest]
+    public class CodedUITest1
+    {
+        
+        public CodedUITest1()
+        {
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            StartCalculatorApp();
+                        
+        }
+
+        [TestMethod]
+        public void CodedUITestMethod1()
+        {
+            // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+            this.UIMap.RecordedMethodSum();
+        }
+
+        /*
+        [TestMethod]
+
+        public void TestMethod1()
+        {
+
+            // Launch the Target App
+            Application application = Application.Launch(@"C:\Users\training\Documents\Visual Studio 2013\ApplicationDemo.exe");
+            // Get Handle to the Target App's main windows
+            Window mainAppWindow = application.GetWindow("Add two numbers");
+            // Get the handle to input textboxes & Set the desired values.
+            mainAppWindow.Get<TextBox>(SearchCriteria.ByAutomationId("UITextBox1Edit")).SetValue("20");
+            mainAppWindow.Get<TextBox>(SearchCriteria.ByAutomationId("UITextBox2Edit")).SetValue("30");
+            // Get the handle to the Add button and perform the Click action
+            mainAppWindow.Get<Button>(SearchCriteria.ByAutomationId("addbutton")).Click();
+            // Get the handle to the Result textbox and the text value
+            var result = mainAppWindow.Get<TextBox>(SearchCriteria.ByAutomationId("UITextBox3Edit"));
+            // Assert the Actual value from result textbox with expected value.
+            Assert.AreEqual(50, Convert.ToInt32(result.Text));
+            // Close the app window.
+            mainAppWindow.Close();
+        } */
+
+        #region Additional test attributes
+
+        // You can use the following additional attributes as you write your tests:
+
+        ////Use TestInitialize to run code before running each test 
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{        
+        //    // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        //}
+
+        ////Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{        
+        //    // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        //}
+
+        #endregion
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+        private TestContext testContextInstance;
+
+        public UIMap UIMap
+        {
+            get
+            {
+                if ((this.map == null))
+                {
+                    this.map = new UIMap();
+                }
+
+                return this.map;
+            }
+        }
+
+        private UIMap map;
+
+        public void StartCalculatorApp()
+        {
+            System.Diagnostics.Process.Start(@"C:\Users\training\Documents\Visual Studio 2013\ApplicationDemo.exe");
+        }
+    }
+}
---
+++ Osvaldo/CalculatorApp/CalculatorApp/CalculatorControls.cs
@@ -0,0 +1,165 @@
+﻿namespace CalculatorApp.CalculatorControlsClasses
+{
+    using System;
+    using System.Collections.Generic;
+    using System.CodeDom.Compiler;
+    using Microsoft.VisualStudio.TestTools.UITest.Extension;
+    using Microsoft.VisualStudio.TestTools.UITesting;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
+    using Mouse = Microsoft.VisualStudio.TestTools.UITesting.Mouse;
+    using MouseButtons = System.Windows.Forms.MouseButtons;
+    using System.Drawing;
+    using System.Windows.Input;
+    using System.Text.RegularExpressions;
+    using Microsoft.VisualStudio.TestTools.UITesting.WinControls;
+    
+    
+    public partial class CalculatorControls
+    {
+        public WinButton Button0
+      {
+        get
+        {
+            //return UICalculatorWindow.UIItemWindow.UIItem1Button;
+            return UICalculatorWindow.UIItemWindow9.UIItem0Button;
+        }
+      }
+        public WinButton Button1
+        {
+            get
+            {
+                //return UICalculatorWindow.UIItemWindow1.UIItem2Button;
+                return UICalculatorWindow.UIItemWindow.UIItem1Button;
+            }
+        }
+
+        public WinButton Button2
+        {
+            get
+            {
+                //return UICalculatorWindow.UIItemWindow2.UIItem3Button;
+                return UICalculatorWindow.UIItemWindow1.UIItem2Button;
+            }
+        }
+
+        public WinButton Button3
+        {
+            get
+            {
+                //return UICalculatorWindow.UIItemWindow3.UIItem4Button;
+                return UICalculatorWindow.UIItemWindow2.UIItem3Button;
+            }
+        }
+
+        public WinButton Button4
+        {
+            get
+            {
+                //return UICalculatorWindow.UIItemWindow4.UIItem5Button;
+                return UICalculatorWindow.UIItemWindow3.UIItem4Button;
+            }
+        }
+
+        public WinButton Button5
+        {
+            get
+            {
+                //return UICalculatorWindow.UIItemWindow5.UIItem6Button;
+                return UICalculatorWindow.UIItemWindow4.UIItem5Button;
+            }
+        }
+
+        public WinButton Button6
+        {
+            get
+            {
+                //return UICalculatorWindow.UIItemWindow6.UIItem7Button;
+                return UICalculatorWindow.UIItemWindow5.UIItem6Button;
+            }
+        }
+
+        public WinButton Button7
+        {
+            get
+            {
+                //return UICalculatorWindow.UIItemWindow7.UIItem8Button;
+                return UICalculatorWindow.UIItemWindow6.UIItem7Button;
+            }
+        }
+        public WinButton Button8
+        {
+            get
+            {
+                //return UICalculatorWindow.UIItemWindow8.UIItem9Button;
+                return UICalculatorWindow.UIItemWindow7.UIItem8Button;
+            }
+        }
+
+        public WinButton Button9
+        {
+            get
+            {
+                //return UICalculatorWindow.UIItemWindow9.UIItem0Button;
+                return UICalculatorWindow.UIItemWindow8.UIItem9Button;
+            }
+        }
+
+        public WinButton ButtonAdd
+        {
+            get
+            {
+                return UICalculatorWindow.UIItemWindow10.UIAddButton;
+            }
+        }
+
+        public WinButton ButtonSubstract
+        {
+            get
+            {
+                return UICalculatorWindow.UIItemWindow11.UISubtractButton;
+            }
+        }
+
+        public WinButton ButtonMultip
+        {
+            get
+            {
+                return UICalculatorWindow.UIItemWindow12.UIMultiplyButton;
+            }
+        }
+
+        public WinButton ButtonDivision
+        {
+            get
+            {
+                return UICalculatorWindow.UIItemWindow13.UIDivideButton;
+            }
+        }
+
+        public WinButton ButtonEquals
+        {
+            get
+            {
+                return UICalculatorWindow.UIItemWindow14.UIEqualsButton;
+            }
+        }
+
+        public WinButton ButtonClear
+        {
+            get
+            {
+                return UICalculatorWindow.UIItemWindow15.UIClearButton;
+            }
+        }
+
+        public WinText Text
+        {
+            get
+            {
+                return UICalculatorWindow.UIItem0Window.UIItem0Text;
+            }
+        }
+        
+    }
+}
